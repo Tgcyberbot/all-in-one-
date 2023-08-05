be2b2c75bf423db6ed70374757d23c4eb6a43360
Tgcyberbot <141454564+Tgcyberbot@users.noreply.github.com>
2023-08-05 12:00:26 +0530

    config.py

--- config.py
+++ config.py
@@ -6,8 +6,8 @@ import os
 
 class Config(object):
     # get a token from @BotFather
-    BOT_TOKEN = "6047785902:AAGT-_mx4Gx-GK3zUshtrqmbjUPxoGx0NF8"
-    API_ID = "24250238"
-    API_HASH = "cb3f118ce5553dc140127647edcf3720"
-    AUTH_USERS = '6175650047'
+    BOT_TOKEN = "6468754006:AAEhkqsoQ1Y-gQIgpPcEA2gNAlt96DKO-9g"
+    API_ID = "24131567"
+    API_HASH = "7bf12fc2ec81ba350bca34c014549bee"
+    AUTH_USERS = '427348392'
 
